e82f27dcb2bebcd549197b2485353c57f55cd8ab
Mardon <markayev9797@gmail.com>
2025-06-28 09:23:24 -0700

    created todo.service

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,26 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
 
 @Injectable()
 export class TodoService {
-  create(createTodoDto: CreateTodoDto) {
-    return 'This action adds a new todo';
+  private todos: Todo[] = [];
+  private id: number = 1;
+
+  create(dto: CreateTodoDto): Todo {
+    const newTodo: Todo = {
+      id: this.id++,
+      title: dto.title,
+      description: dto.description,
+      isDone: false,
+    };
+    this.todos.push(newTodo);
+    return newTodo;
   }
 
-  findAll() {
-    return `This action returns all todo`;
+  findAll(): Todo[] {
+    return this.todos;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} todo`;
+  findOne(id: number): Todo {
+    const todo = this.todos.find((t) => t.id === id);
+    if (!todo) {
+      throw new NotFoundException('Todo not found');
+    }
+    return todo;
   }
 
-  update(id: number, updateTodoDto: UpdateTodoDto) {
-    return `This action updates a #${id} todo`;
+  update(id: number, dto: UpdateTodoDto): Todo {
+    const todo = this.findOne(id);
+    Object.assign(todo, dto);
+    return todo;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} todo`;
+  remove(id: number): void {
+    const index = this.todos.findIndex((t) => t.id === id);
+    if (index === -1) {
+      throw new NotFoundException('Not found id');
+    }
+    this.todos.splice(index, 1);
   }
 }
